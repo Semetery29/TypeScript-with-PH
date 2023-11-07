9c8a30ac0ade75676dd17f27ce6f77b635fc0b22
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-11-07 21:47:01 +0600

    3.5 - Access Modifiers

---
+++ typescript-technocrat/module3/3.5.ts
@@ -0,0 +1,36 @@
+{
+  // access modifiers
+
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    /*private*/ protected _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    public addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    public getBalance(){
+      return this._balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount{
+    test(){
+    }
+  }
+
+  const goribManusherAccount = new BankAccount(111, "Mr. Gorib", 20);
+  // goribManusherAccount.balance = 0;
+  goribManusherAccount.addDeposit(20);
+  const myBalance = goribManusherAccount.getBalance();
+  console.log(myBalance);
+
+  //
+}
